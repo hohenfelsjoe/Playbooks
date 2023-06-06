a2c112d2b71326860bfa6791c8e6c35dd314aae6
hohenfelsjoe <61366123+hohenfelsjoe@users.noreply.github.com>
2023-06-06 12:53:24 -0500

    More More More said the private

--- pi/scripts/attic/temp.sh
+++ pi/scripts/attic/temp.sh
@@ -27,65 +27,65 @@ rrdtool update house_temp.rrd $DATE:$TEMP
 rrdtool update attic_humid.rrd $DATE:$HUMID
 
 # Create the Daily Graphs
-rrdtool graph /etc/temperatures/house_temp_day.png -s -1day DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
-rrdtool graph /etc/temperatures/attic_humid_day.png -s -1day DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/house_temp_day.png -s -1day DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/attic_humid_day.png -s -1day DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/rm /mnt/share/Temperatures/house*.png
 /bin/rm /mnt/share/Temperatures/attic*.png
 /bin/cp /etc/temperatures/house_temp_day.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/attic_humid_day.png /mnt/share/Temperatures/
 
 #create weekly graph
-rrdtool graph /etc/temperatures/house_temp_week.png -s -1week DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
-rrdtool graph /etc/temperatures/attic_humid_week.png -s -1week DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/house_temp_week.png -s -1week DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/attic_humid_week.png -s -1week DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/house_temp_week.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/attic_humid_week.png /mnt/share/Temperatures/
 
 #create monthly graph
-rrdtool graph /etc/temperatures/house_temp_month.png -s -1month DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
-rrdtool graph /etc/temperatures/attic_humid_month.png -s -1month DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/house_temp_month.png -s -1month DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/attic_humid_month.png -s -1month DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/house_temp_month.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/attic_humid_month.png /mnt/share/Temperatures/
 
 #create yearly graph
-rrdtool graph /etc/temperatures/house_temp_year.png -s -1year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
-rrdtool graph /etc/temperatures/attic_humid_year.png -s -1year DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/house_temp_year.png -s -1year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/attic_humid_year.png -s -1year DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/house_temp_year.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/attic_humid_year.png /mnt/share/Temperatures/
 
 #create 2 year graph
-rrdtool graph /etc/temperatures/house_temp_2year.png -s -2year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/attic_humid_2year.png -s -2year DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/house_temp_2year.png -s -2year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/attic_humid_2year.png -s -2year DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/house_temp_2year.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/attic_humid_2year.png /mnt/share/Temperatures/
 
 #create 5 year graph
-rrdtool graph /etc/temperatures/house_temp_5year.png -s -5year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/attic_humid_5year.png -s -5year DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/house_temp_5year.png -s -5year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/attic_humid_5year.png -s -5year DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#e600e6:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/house_temp_5year.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/attic_humid_5year.png /mnt/share/Temperatures/
 
 #Create the combined daily graph
-rrdtool graph /etc/temperatures/local_combined_attic_day.png -s -1day DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_attic_day.png -s -1day DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_attic_day.png /mnt/share/Temperatures/
 
 #Create the combined weekly graph
-rrdtool graph /etc/temperatures/local_combined_attic_week.png -s -1week DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_attic_week.png -s -1week DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_attic_week.png /mnt/share/Temperatures/
 
 #Create the combined monthly graph
-rrdtool graph /etc/temperatures/local_combined_attic_month.png -s -1month DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_attic_month.png -s -1month DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_attic_month.png /mnt/share/Temperatures/
 
 #Create the combined yearly graph
-rrdtool graph /etc/temperatures/local_combined_attic_year.png -s -1year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_attic_year.png -s -1year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_attic_year.png /mnt/share/Temperatures/
 
 #Create the combined 2 year graph
-rrdtool graph /etc/temperatures/local_combined_attic_2year.png -s -2year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_attic_2year.png -s -2year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_attic_2year.png /mnt/share/Temperatures/
 
 #Create the combined 5 year graph
-rrdtool graph /etc/temperatures/local_combined_attic_5year.png -s -5year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_attic_5year.png -s -5year DEF:house_temp=house_temp.rrd:temp:AVERAGE LINE1:house_temp#e600e6:"Attic Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:attic_humid=attic_humid.rrd:temp:AVERAGE LINE1:attic_humid#ff0000:"Attic Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_attic_5year.png /mnt/share/Temperatures/
 
 #Copy RRD to Share
--- pi/scripts/barn/temp.sh
+++ pi/scripts/barn/temp.sh
@@ -27,63 +27,63 @@ rrdtool update barn_temp.rrd $DATE:$TEMP
 rrdtool update barn_humid.rrd $DATE:$HUMID
 
 # Create the Daily Graphs
-rrdtool graph /etc/temperatures/barn_temp_day.png -s -1day DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
-rrdtool graph /etc/temperatures/barn_humid_day.png -s -1day DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/barn_temp_day.png -s -1day DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/barn_humid_day.png -s -1day DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/barn_temp_day.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/barn_humid_day.png /mnt/share/Temperatures/
 
 #create weekly graph
-rrdtool graph /etc/temperatures/barn_temp_week.png -s -1week DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/barn_humid_week.png -s -1week DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/barn_temp_week.png -s -1week DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/barn_humid_week.png -s -1week DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/barn_temp_week.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/barn_humid_week.png /mnt/share/Temperatures/
 
 #create monthly graph
-rrdtool graph /etc/temperatures/barn_temp_month.png -s -1month DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/barn_humid_month.png -s -1month DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/barn_temp_month.png -s -1month DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/barn_humid_month.png -s -1month DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/barn_temp_month.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/barn_humid_month.png /mnt/share/Temperatures/
 
 #create yearly graph
-rrdtool graph /etc/temperatures/barn_temp_year.png -s -1year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/barn_humid_year.png -s -1year DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/barn_temp_year.png -s -1year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/barn_humid_year.png -s -1year DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/barn_temp_year.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/barn_humid_year.png /mnt/share/Temperatures/
 
 #Create 2 Year graph
-rrdtool graph /etc/temperatures/barn_temp_2year.png -s -2year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/barn_humid_2year.png -s -2year DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/barn_temp_2year.png -s -2year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/barn_humid_2year.png -s -2year DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/barn_temp_2year.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/barn_humid_2year.png /mnt/share/Temperatures/
 
 #Create 5 Year graph
-rrdtool graph /etc/temperatures/barn_temp_5year.png -s -5year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/barn_humid_5year.png -s -5year DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/barn_temp_5year.png -s -5year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/barn_humid_5year.png -s -5year DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/barn_temp_5year.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/barn_humid_5year.png /mnt/share/Temperatures/
 
 #Create the combined daily graph
-rrdtool graph /etc/temperatures/local_combined_barn_day.png -s -1day DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_barn_day.png -s -1day DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_barn_day.png /mnt/share/Temperatures/
 
 #Create the combined weekly graph
-rrdtool graph /etc/temperatures/local_combined_barn_week.png -s -1week DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_barn_week.png -s -1week DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_barn_week.png /mnt/share/Temperatures/
 
 #Create the combined monthly graph
-rrdtool graph /etc/temperatures/local_combined_barn_month.png -s -1month DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_barn_month.png -s -1month DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#ff0000:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_barn_month.png /mnt/share/Temperatures/
 
 #Create the combined yearly graph
-rrdtool graph /etc/temperatures/local_combined_barn_1year.png -s -1year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#4d2600 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_barn_1year.png -s -1year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#4d2600:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_barn_1year.png /mnt/share/Temperatures/
 
 #Create the combined 2year graph
-rrdtool graph /etc/temperatures/local_combined_barn_2year.png -s -2year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#4d2600 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_barn_2year.png -s -2year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#4d2600:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_barn_2year.png /mnt/share/Temperatures/
 
 #Create the combined 5year graph
-rrdtool graph /etc/temperatures/local_combined_barn_5year.png -s -5year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#4d2600 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_barn_5year.png -s -5year DEF:barn_temp=barn_temp.rrd:temp:AVERAGE LINE1:barn_temp#4d2600:"Shop Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:barn_humid=barn_humid.rrd:temp:AVERAGE LINE1:barn_humid#ff0000:"Shop Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_barn_5year.png /mnt/share/Temperatures/
 
 #Copy RRD to Share
