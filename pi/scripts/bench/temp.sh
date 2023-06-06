9607cc5f81f298e4fd09911b134d13cb9c3c76a8
hohenfelsjoe <61366123+hohenfelsjoe@users.noreply.github.com>
2023-06-06 13:39:53 -0500

    shop

--- pi/scripts/bench/temp.sh
+++ pi/scripts/bench/temp.sh
@@ -27,63 +27,63 @@ rrdtool update bench_temp.rrd $DATE:$TEMP
 rrdtool update bench_humid.rrd $DATE:$HUMID
 
 # Create the Daily Graphs
-rrdtool graph /etc/temperatures/armory_temp_day.png -s -1day DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
-rrdtool graph /etc/temperatures/armory_humid_day.png -s -1day DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/armory_temp_day.png -s -1day DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/armory_humid_day.png -s -1day DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/armory_temp_day.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/armory_humid_day.png /mnt/share/Temperatures/
 
 #create weekly graph
-rrdtool graph /etc/temperatures/armory_temp_week.png -s -1week DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/armory_humid_week.png -s -1week DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/armory_temp_week.png -s -1week DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/armory_humid_week.png -s -1week DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/armory_temp_week.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/armory_humid_week.png /mnt/share/Temperatures/
 
 #create monthly graph
-rrdtool graph /etc/temperatures/armory_temp_month.png -s -1month DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/armory_humid_month.png -s -1month DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/armory_temp_month.png -s -1month DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/armory_humid_month.png -s -1month DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/armory_temp_month.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/armory_humid_month.png /mnt/share/Temperatures/
 
 #create yearly graph
-rrdtool graph /etc/temperatures/armory_temp_year.png -s -1year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/armory_humid_year.png -s -1year DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/armory_temp_year.png -s -1year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/armory_humid_year.png -s -1year DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/armory_temp_year.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/armory_humid_year.png /mnt/share/Temperatures/
 
 #Create 2 Year graph
-rrdtool graph /etc/temperatures/armory_temp_2year.png -s -2year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/armory_humid_2year.png -s -2year DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/armory_temp_2year.png -s -2year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/armory_humid_2year.png -s -2year DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/armory_temp_2year.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/armory_humid_2year.png /mnt/share/Temperatures/
 
 #Create 5 Year graph
-rrdtool graph /etc/temperatures/armory_temp_5year.png -s -5year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
-rrdtool graph /etc/temperatures/armory_humid_5year.png -s -5year DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
+rrdtool graph /etc/temperatures/armory_temp_5year.png -s -5year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/armory_humid_5year.png -s -5year DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#d27d2d:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa 
 /bin/cp /etc/temperatures/armory_temp_5year.png /mnt/share/Temperatures/
 /bin/cp /etc/temperatures/armory_humid_5year.png /mnt/share/Temperatures/
 
 #Create the combined daily graph
-rrdtool graph /etc/temperatures/local_combined_armory_day.png -s -1day DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_armory_day.png -s -1day DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_armory_day.png /mnt/share/Temperatures/
 
 #Create the combined weekly graph
-rrdtool graph /etc/temperatures/local_combined_armory_week.png -s -1week DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_armory_week.png -s -1week DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_armory_week.png /mnt/share/Temperatures/
 
 #Create the combined monthly graph
-rrdtool graph /etc/temperatures/local_combined_armory_month.png -s -1month DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_armory_month.png -s -1month DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_armory_month.png /mnt/share/Temperatures/
 
 #Create the combined yearly graph
-rrdtool graph /etc/temperatures/local_combined_armory_year.png -s -1year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_armory_year.png -s -1year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_armory_year.png /mnt/share/Temperatures/
 
 #Create the combined 2 year graph
-rrdtool graph /etc/temperatures/local_combined_armory_2year.png -s -2year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_armory_2year.png -s -2year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_armory_2year.png /mnt/share/Temperatures/
 
 #Create the combined 5 year graph
-rrdtool graph /etc/temperatures/local_combined_armory_5year.png -s -5year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000 -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
+rrdtool graph /etc/temperatures/local_combined_armory_5year.png -s -5year DEF:bench_temp=bench_temp.rrd:temp:AVERAGE LINE1:bench_temp#d27d2d:"Office Temp" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa DEF:bench_humid=bench_humid.rrd:temp:AVERAGE LINE1:bench_humid#ff0000:"Office Humidity" -h 400 -w 600 -y1:2 --color GRID#dddddd --color MGRID#aaaaaa
 /bin/cp /etc/temperatures/local_combined_armory_5year.png /mnt/share/Temperatures/
 
 #Copy RRD to Share
